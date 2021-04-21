0c191d06847d1969bcf37254e898596b020cdf98
raider2107 <3108987378@qq.com>
2021-04-21 16:41:27 +0800

    fix banner

--- taxi-station-website/js/main.js
+++ taxi-station-website/js/main.js
@@ -101,3 +101,25 @@ document.querySelector(".scroll-btn").addEventListener("click", () => {
 // //   currentDot.classList.remove("current-slide");
 // //   targetDot.classList.add("current-slide");
 // // });
+
+// jQuery(document).ready(function () {
+//   $(".hero-text").mousemove(function (e) {
+//     var rXP = e.pageX - this.offsetLeft - $(this).width() / 2;
+//     var rYP = e.pageY - this.offsetTop - $(this).height() / 2;
+//     $(".hero-text").css(
+//       "text-shadow",
+//       +rYP / 10 +
+//         "px " +
+//         rXP / 80 +
+//         "px rgba(227,6,19,.8), " +
+//         rYP / 8 +
+//         "px " +
+//         rXP / 60 +
+//         "px rgba(255,237,0,1), " +
+//         rXP / 70 +
+//         "px " +
+//         rYP / 12 +
+//         "px rgba(0,159,227,.7)"
+//     );
+//   });
+// });
